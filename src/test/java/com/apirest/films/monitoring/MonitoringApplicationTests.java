93f3b00da56dfbd07cf47268d0228f56abf4233c
Adriano Saadeh <adrianosaadeh@gmail.com>
2020-10-25 14:42:24 -0300

    first commit

---
+++ src/test/java/com/apirest/films/monitoring/MonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apirest.films.monitoring;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
